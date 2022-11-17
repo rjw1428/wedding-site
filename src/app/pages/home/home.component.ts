a3ee8c72de84a9bb49ae810caafaddcc3deb3085
Ryan Wilk <rjw1428@gmail.com>
2022-11-16 22:30:05 -0500

    [Home] multi images on full screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { distinctUntilChanged, fromEvent, map, startWith } from 'rxjs';
+
+
+const IMG_COUNT = 4
+const MAX_IMG_WIDTH = 500
 
 @Component({
   selector: 'app-home',
@@ -7,15 +12,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-
+  imagePool = new Array(IMG_COUNT).fill(0).map((_, i) => `/assets/images/homepic${i+1}.jpg`).sort(() => Math.random() < .5 ? 1 : -1)
+  image$ = fromEvent(window, 'resize').pipe(
+    map(event => {
+      const target = event.target as Window
+      return target.innerWidth
+    }),
+    startWith(window.innerWidth),
+    map(width => {
+      const num = Math.ceil(width / MAX_IMG_WIDTH)
+      return num > IMG_COUNT ? IMG_COUNT : num
+    }),
+    distinctUntilChanged(),
+    map(numOfImages => this.imagePool.slice(0, numOfImages)),
+  )
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getImage() {
-    const num = Math.ceil(Math.random()*4)
-    return {backgroundImage: `url('/assets/images/homepic${num}.jpg')`};
+  getImage(img: string) {
+    return {backgroundImage: `url(${img})`};
   }
 
 }

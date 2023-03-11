d12fc40fe658112a563c76a8d72dcbeb52e17c15
Ryan Wilk <rjw1428@gmail.com>
2023-03-11 00:22:03 -0500

    Added ngrxLet and updated cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
 admin.initializeApp();
 
 export const createSearch = functions.firestore.document('/guests/{documentId}')
@@ -8,14 +7,15 @@ export const createSearch = functions.firestore.document('/guests/{documentId}')
     const data = snapshot.data() as {
       primary: {firstName: string, lastName: string};
       secondary?: {firstName: string, lastName: string};
+      searchExtra: string[]
     };
-    const search = [];
+    const search = data.searchExtra ? data.searchExtra : [];
     search.push(data.primary.firstName.toLowerCase());
     search.push(data.primary.lastName.toLowerCase());
     if (data.secondary) {
       search.push(data.secondary.firstName.toLowerCase());
       search.push(data.secondary.lastName.toLowerCase());
     }
-
+    console.log(`handling for ${snapshot.id}`)
     return snapshot.ref.set({search}, {merge: true});
   });
